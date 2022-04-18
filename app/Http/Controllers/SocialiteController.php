76b27d1fe69d8841bb4b52a097d6b47cb2a94517
iSonnet7 <paco82009@gmail.com>
2022-04-18 18:09:42 -0500

    Login Google

--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -33,8 +33,7 @@ class SocialiteController extends Controller
 
             //Auth::login($new);
 
-            return redirect()->route('home');
-            
+            return redirect()->route('home', compact('name'));
         }
     }
 }
